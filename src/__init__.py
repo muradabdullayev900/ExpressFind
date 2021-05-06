695f50230ee03df8c41d31eb63879928e5b52077
muradabdullayev900 <murad.abdullayev.std@bhos.edu.az>
2021-05-07 02:15:54 +0400

    updated code structure

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-# from amazon_scrape import *
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '305e2c4a398c87c734c234c395c2aa46'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
 from src import routes
--- src/routes.py
+++ src/routes.py
@@ -1,48 +1,40 @@
 from src import app
-from flask import render_template, url_for, flash, redirect
-from src.forms import *
-from src.amazon_scrape import *
-from src.tapaz_scrape import *
-from src.aliexpress_scrape import *
-
+from flask import render_template, request
+from src.forms import SearchForm
+from src.amazon_filter import *
+from src.tapaz_filter import *
+from src.aliexpress_filter import *
+from src.temp import Threading
 
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    amazon_product = []
+    tapaz_product = []
+    aliexpress_product = []
     form = SearchForm()
     item = form.item.data
-    if item:
-        amazon_product = amazon_scraper(item)
-        tapaz_product = tapaz_scraper(item)
-        aliexpress_product = aliexpress_scraper(item)
+    min_price = form.min_price.data
+    max_price = form.max_price.data
+    if request.method == 'POST':
+        sort = request.form['sort']
+        currency = request.form['currency']
+        amazon = request.form.getlist('amazon')
+        tapaz = request.form.getlist('tapaz')
+        aliexpress = request.form.getlist('aliexpress')
+        # amazon_product, tapaz_product, aliexpress_product = Threading().threading(item, amazon, tapaz, aliexpress)
+        if amazon:
+            amazon_product = AmazonFilter().Filter(item, sort, currency, min_price, max_price)
+        if tapaz:
+            tapaz_product = TapazFilter().Filter(item, sort, currency, min_price, max_price)
+        if aliexpress:
+            aliexpress_product = AliexpressFilter().Filter(item, sort, currency, min_price, max_price)
+
+        return render_template('home.html', title='Search', form=form, amazon_product=amazon_product, tapaz_product=tapaz_product, aliexpress_product=aliexpress_product)
     else:
-        amazon_product = {}
-        tapaz_product = {}
-        aliexpress_product = {}
-    return render_template('home.html', title='Search', form=form) # , aliexpress_product=aliexpress_product) # amazon_product=amazon_product, tapaz_product=tapaz_product, )
+        return render_template('home.html', title='Search', form=form, amazon_product=amazon_product, tapaz_product=tapaz_product, aliexpress_product=aliexpress_product) # , aliexpress_product=aliexpress_product) # amazon_product=amazon_product, tapaz_product=tapaz_product, )
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in successfully', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
